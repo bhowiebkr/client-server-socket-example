8ddd7f442bcfd823f845b1c7f33b20981ece74b7
Bryan <bhowiebkr@gmail.com>
2023-09-21 23:50:14 -0400

    start over using QtNetwork instead of python socket. Much cleaner!

--- client.py
+++ client.py
@@ -1,7 +1,4 @@
-# Import necessary modules
-import errno
 import sys
-import socket
 from PySide6.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -9,148 +6,84 @@ from PySide6.QtWidgets import (
     QPushButton,
     QVBoxLayout,
     QWidget,
-    QFormLayout,
     QLineEdit,
+    QFormLayout,
 )
-from PySide6.QtCore import QObject, QThread, Signal
+from PySide6.QtNetwork import QTcpSocket, QHostAddress
+
+go_dark = False
+try:
+    import qdarktheme
+
+    go_dark = True
+except Exception:
+    pass
 
 
-# Create a class for the client-side logic
-class ClientClient(QObject):
-    messageReceived = Signal(str)  # Signal to emit received messages
-    errorOccurred = Signal(str)  # Signal to emit error messages
-
+class Client(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.initUI()
+        self.socket = QTcpSocket(self)
+        self.socket.connected.connect(self.connected)
+        self.socket.readyRead.connect(self.receiveMessage)
 
-        self.client_socket = None  # Socket object for communication
-        self.running = True  # Flag to track the client's running state
-
-    # Helper method to get the local IP address
-    def lookup_ip(self) -> str:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.connect(("8.8.8.8", 80))
-        address = sock.getsockname()[0]
-        sock.close()
-        return address
-
-    # Method to establish a socket connection to a server
-    def connect_socket(self) -> bool:
-        try:
-            self.client_socket = socket.socket()
-            self.client_socket.connect((self.lookup_ip(), 1234))  # Connect to a server on port 1234
-            return True
-        except socket.error as error:
-            self.errorOccurred.emit(f"Connection error: {str(error)}")
-            return False
-
-    # Method to send a message and receive a response from the server
-    def send_receive(self, msg: str) -> str:
-        if not self.client_socket:
-            self.errorOccurred.emit("Socket is not connected")
-            return ""
-
-        if not msg:
-            self.errorOccurred.emit("Empty message")
-            return ""
-
-        try:
-            self.client_socket.sendall(msg.encode("utf-8"))
-            recv = self.client_socket.recv(1024).decode("utf-8")
-            self.messageReceived.emit(recv)  # Emit the received message
-            return recv
-        except socket.error as error:
-            self.errorOccurred.emit(f"Socket error: {str(error)}")
-            return ""
-
-    # Method to stop the client and close the socket
-    def stop(self):
-        if self.client_socket:
-            print("Stopping client")
-            self.client_socket.close()
-        else:
-            print("No client yet")
-
-
-# Create the main window for the application
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+    def initUI(self):
+        self.setGeometry(600, 100, 400, 300)
         self.setWindowTitle("Client")
 
-        self.central_widget = QWidget()
+        # Layouts
         self.layout = QVBoxLayout()
-
-        self.text_edit = QTextEdit()
-        self.layout.addWidget(self.text_edit)
-
+        self.setLayout(self.layout)
+        central_widget = QWidget()
+        central_widget.setLayout(self.layout)
+        self.setCentralWidget(central_widget)
         form = QFormLayout()
-        self.input = QLineEdit()
-        form.addRow("Input", self.input)
+
+        # Widgets
+        connect_btn = QPushButton("Connect")
+        connect_btn.clicked.connect(self.connect)
+
+        self.text_edit = QTextEdit(self)
+        self.text_edit.setReadOnly(True)  # Set it to read-only for history
+
+        self.input_line = QLineEdit(self)
+        self.input_line.returnPressed.connect(self.sendMessage)  # Send when Return key is pressed
+
+        # Add Widgets
+        form.addRow("Message:", self.input_line)
+        self.layout.addWidget(self.text_edit)
         self.layout.addLayout(form)
+        self.layout.addWidget(connect_btn)
 
-        # Create buttons to start and stop the client
-        self.start_button = QPushButton("Start Client")
-        self.start_button.clicked.connect(self.start_client)
-        self.layout.addWidget(self.start_button)
+    def connect(self):
+        self.socket.connectToHost(QHostAddress.LocalHost, 12345)
 
-        self.stop_button = QPushButton("Stop Client")
-        self.stop_button.clicked.connect(self.stop_client)
-        self.layout.addWidget(self.stop_button)
+    def connected(self):
+        self.text_edit.append("Connected to server.")
 
-        # Set the layout for the central widget
-        self.central_widget.setLayout(self.layout)
-        self.setCentralWidget(self.central_widget)
+    def sendMessage(self):
+        message = self.input_line.text()
+        if message:
+            self.text_edit.append(f"Sent: {message}")
+            self.socket.write(message.encode())
+            self.input_line.clear()  # Clear the input field after sending
 
-        # Create a client instance and move it to a separate thread
-        self.client_thread = QThread()
-        self.client = ClientClient()
-        self.client.moveToThread(self.client_thread)
-        self.client_thread.started.connect(self.client.connect_socket)
-        self.client_thread.start()
-
-        # Connect UI signals to client methods
-        self.input.returnPressed.connect(self.send_msg)
-        self.client.messageReceived.connect(self.display_message)
-        self.client.errorOccurred.connect(self.display_error)
-
-    # Method to send a message when the user presses Enter
-    def send_msg(self):
-        msg = self.input.text()
-        self.client.send_receive(msg)
-        self.input.clear()
-
-    # Method to start the client when the "Start Client" button is clicked
-    def start_client(self):
-        if not self.client.connect_socket():
-            self.display_error("Failed to connect to the server")
-        else:
-            self.display_message("Client started")
-
-    # Method to stop the client when the "Stop Client" button is clicked
-    def stop_client(self):
-        self.client.stop()
-        self.client_thread.quit()
-        self.client_thread.wait()
-        self.display_message("Client stopped")
-
-    # Method to display received messages in the text edit area
-    def display_message(self, message):
-        self.text_edit.append(message)
-
-    # Method to display error messages in the text edit area
-    def display_error(self, error_message):
-        self.text_edit.append(f"Error: {error_message}")
+    def receiveMessage(self):
+        message = self.socket.readAll().data().decode()
+        self.text_edit.append(f"Received: {message}")
 
 
-# Main function to start the application
 def main():
     app = QApplication(sys.argv)
-    window = MyWindow()
-    window.show()
+
+    if go_dark:
+        qdarktheme.setup_theme(additional_qss="QToolTip {color: black;}")
+
+    client = Client()
+    client.show()
     sys.exit(app.exec())
 
 
-# Check if the script is being run as the main program
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,152 +1,55 @@
-# Import necessary modules
-import socket
-import errno
-from PySide6.QtWidgets import (
-    QApplication,
-    QMainWindow,
-    QTextEdit,
-    QPushButton,
-    QVBoxLayout,
-    QWidget,
-)
-from PySide6.QtCore import QObject, QThread, Signal
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QTextEdit, QVBoxLayout, QWidget
+from PySide6.QtNetwork import QTcpServer, QHostAddress
+
+go_dark = False
+try:
+    import qdarktheme
+
+    go_dark = True
+except Exception:
+    pass
 
 
-# Create a class for the server-side logic
-class SocketServer(QObject):
-    messageReceived = Signal(str)  # Signal to emit received messages
-    errorOccurred = Signal(str)  # Signal to emit error messages
-
+class Server(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.server_socket = None  # Socket object for server
-        self.running = False  # Flag to track server's running state
+        self.initUI()
+        self.server = QTcpServer(self)
+        self.server.listen(QHostAddress.LocalHost, 12345)
+        self.server.newConnection.connect(self.newConnection)
 
-    # Helper method to get the local IP address
-    def lookup_ip(self) -> str:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.connect(("8.8.8.8", 80))
-        address = sock.getsockname()[0]
-        sock.close()
-        return address
-
-    # Method to start the server and listen for incoming connections
-    def run(self):
-        try:
-            self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.server_socket.bind((self.lookup_ip(), 1234))  # Bind to a specific IP and port
-            self.server_socket.listen(1)
-            self.running = True
-
-            while self.running:
-                client_socket, _ = self.server_socket.accept()
-                self.handle_client(client_socket)
-        except socket.error as error:
-            self.errorOccurred.emit(f"Server error: {str(error)}")
-
-    # Method to handle a client's connection
-    def handle_client(self, client_socket):
-        with client_socket:
-            while self.running:
-                try:
-                    data = client_socket.recv(1024).decode("utf-8")
-                    if not data:
-                        break  # Client disconnected
-                    client_socket.sendall(bytes(f"I got the message: {data}", "ascii"))
-
-                    self.messageReceived.emit(data)  # Emit the received message
-                except socket.error as error:
-                    if error.errno in (errno.WSAECONNRESET, errno.WSAECONNABORTED):
-                        self.errorOccurred.emit("Client connection reset or aborted")
-                        break
-                    else:
-                        self.errorOccurred.emit(f"Socket error: {str(error)}")
-
-    # Method to stop the server and close the socket
-    def stop(self):
-        print("Stopping server...")
-        self.running = False
-        if self.server_socket:
-            self.server_socket.close()
-
-    # Method to start the server
-    def start_server(self):
-        if not self.running:
-            self.server_thread = QThread()
-            self.moveToThread(self.server_thread)
-            self.server_thread.started.connect(self.run)
-            self.server_thread.start()
-
-
-# Create the main window for the application
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+    def initUI(self):
+        self.setGeometry(100, 100, 400, 300)
         self.setWindowTitle("Server")
+        self.text_edit = QTextEdit(self)
+        self.text_edit.setReadOnly(True)  # Set it to read-only for history
+        central_widget = QWidget()
+        central_layout = QVBoxLayout()
+        central_layout.addWidget(self.text_edit)
+        central_widget.setLayout(central_layout)
+        self.setCentralWidget(central_widget)
 
-        self.central_widget = QWidget()
-        self.layout = QVBoxLayout()
+    def newConnection(self):
+        self.client_connection = self.server.nextPendingConnection()
+        self.client_connection.readyRead.connect(self.receiveMessage)
 
-        self.text_edit = QTextEdit()
-        self.layout.addWidget(self.text_edit)
-
-        # Create buttons to start and stop the server
-        self.start_button = QPushButton("Start Server")
-        self.start_button.clicked.connect(self.start_server)
-        self.layout.addWidget(self.start_button)
-
-        self.stop_button = QPushButton("Stop Server")
-        self.stop_button.clicked.connect(self.stop_server)
-        self.layout.addWidget(self.stop_button)
-
-        # Set the layout for the central widget
-        self.central_widget.setLayout(self.layout)
-        self.setCentralWidget(self.central_widget)
-
-        self.server_thread = QThread()
-        self.server = SocketServer()
-        self.server.messageReceived.connect(self.display_message)
-        self.server.errorOccurred.connect(self.display_error)
-
-        # Connect the server's start_server method to the thread's started signal
-        self.server_thread.started.connect(self.server.start_server)
-
-    # Method to start the server when the "Start Server" button is clicked
-    def start_server(self):
-        print("Starting server")
-        self.server.moveToThread(self.server_thread)
-        self.server_thread.start()
-        print("Server started")
-
-    # Method to stop the server when the "Stop Server" button is clicked
-    def stop_server(self):
-        print("Stopping Server")
-        self.server.stop()
-
-        self.server_thread.quit()
-        self.server_thread.wait()
-
-        print("Server Stopped")
-
-    # Method to display received messages in the text edit area
-    def display_message(self, message):
-        print(f"Adding message: {message}")
-        self.text_edit.append(message)
-
-    # Method to display error messages in the text edit area
-    def display_error(self, error_message):
-        self.text_edit.append(f"Error: {error_message}")
+    def receiveMessage(self):
+        message = self.client_connection.readAll().data().decode()
+        self.text_edit.append(f"Received: {message}")
+        self.client_connection.write(f"Server received: {message}".encode())
 
 
-# Main function to start the application
 def main():
-    app = QApplication([])
-    window = MyWindow()
-    window.show()
-    app.exec()
+    app = QApplication(sys.argv)
+
+    if go_dark:
+        qdarktheme.setup_theme(additional_qss="QToolTip {color: black;}")
+
+    server = Server()
+    server.show()
+    sys.exit(app.exec())
 
 
-# Check if the script is being run as the main program
 if __name__ == "__main__":
     main()
